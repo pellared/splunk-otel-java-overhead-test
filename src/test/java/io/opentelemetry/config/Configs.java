5caa0bdeaf877cd1a882f9473c20d861d68f348c
jason plumb <75337021+breedx-splk@users.noreply.github.com>
2022-08-01 16:04:21 -0700

    slight increase before full rollback (#104)

--- src/test/java/io/opentelemetry/config/Configs.java
+++ src/test/java/io/opentelemetry/config/Configs.java
@@ -23,9 +23,9 @@ public enum Configs {
   RELEASE(TestConfig.builder()
       .name("release_30vu_8500iter")
       .description("multiple agent configurations compared")
-      .withAgents(Agents.NONE, Agents.SPLUNK_OTEL)//, Agents.SPLUNK_1_13, Agents.SPLUNK_1_14, Agents.OTEL)
+      .withAgents(Agents.NONE, Agents.SPLUNK_OTEL, Agents.SPLUNK_1_13, Agents.SPLUNK_1_14, Agents.OTEL)
       .totalIterations(500)
-//      .warmupSeconds(60)
+      .warmupSeconds(5)
       .maxRequestRate(900)
       .concurrentConnections(30)
       .build());
